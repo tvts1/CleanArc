3b301bdab08fa217575195c755bd7e149ec6fae6
TassioTristao <tassio.tristao05@gmail.com>
2025-05-21 10:13:23 -0300

    Initial commit

---
+++ src/test/java/com/study/EventCelan/EventCelanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.EventCelan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCelanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
